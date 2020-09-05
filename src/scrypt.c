bf74c81c0f625a2bca408f93251b540ecc6c2e7c
Dani Biró <danibiro93@gmail.com>
2020-09-05 14:21:19 +0300

    Format C code

--- src/scrypt.c
+++ src/scrypt.c
@@ -27,7 +27,7 @@
  * online backup system.
  *
  * Modified for hash-wasm by Dani Biró
-*/
+ */
 
 #include <emscripten.h>
 #include <stdint.h>
@@ -64,7 +64,8 @@ uint8_t *Hash_GetBuffer() {
   if (B == NULL) {
     // start of new memory
     B = (uint8_t *)(__builtin_wasm_memory_size(0) * BYTES_PER_PAGE);
-    if (Hash_SetMemorySize(512 * 1024) == -1) { // always preallocate 16kb to not cause problems with the other hashes
+    // always preallocate 16kb to not cause problems with the other hashes
+    if (Hash_SetMemorySize(512 * 1024) == -1) {
       return NULL;
     }
   }
@@ -72,62 +73,71 @@ uint8_t *Hash_GetBuffer() {
   return B;
 }
 
-static inline uint32_t
-le32dec(const void * pp)
-{
-  return ((uint32_t*)pp)[0];
+static inline uint32_t le32dec(const void *pp) {
+  return ((uint32_t *)pp)[0];
 }
 
-static inline void
-le32enc(void * pp, uint32_t x)
-{
-  ((uint32_t*)pp)[0] = x;
+static inline void le32enc(void *pp, uint32_t x) {
+  ((uint32_t *)pp)[0] = x;
 }
-
 
 /**
  * salsa20_8(B):
  * Apply the salsa20/8 core to the provided block.
  */
-static void
-salsa20_8(uint32_t B[16])
-{
+static void salsa20_8(uint32_t B[16]) {
   uint32_t x[16];
 
   #pragma clang loop unroll(full)
   for (uint8_t i = 0; i < 8; i++) {
-    ((uint64_t*)x)[i] = ((uint64_t*)B)[i];
+    ((uint64_t *)x)[i] = ((uint64_t *)B)[i];
   }
 
   #pragma clang loop unroll(full)
   for (uint8_t i = 0; i < 8; i += 2) {
-#define R(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
+    #define R(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
     /* Operate on columns. */
-    x[ 4] ^= R(x[ 0]+x[12], 7);  x[ 8] ^= R(x[ 4]+x[ 0], 9);
-    x[12] ^= R(x[ 8]+x[ 4],13);  x[ 0] ^= R(x[12]+x[ 8],18);
+    x[4] ^= R(x[0] + x[12], 7);
+    x[8] ^= R(x[4] + x[0], 9);
+    x[12] ^= R(x[8] + x[4], 13);
+    x[0] ^= R(x[12] + x[8], 18);
 
-    x[ 9] ^= R(x[ 5]+x[ 1], 7);  x[13] ^= R(x[ 9]+x[ 5], 9);
-    x[ 1] ^= R(x[13]+x[ 9],13);  x[ 5] ^= R(x[ 1]+x[13],18);
+    x[9] ^= R(x[5] + x[1], 7);
+    x[13] ^= R(x[9] + x[5], 9);
+    x[1] ^= R(x[13] + x[9], 13);
+    x[5] ^= R(x[1] + x[13], 18);
 
-    x[14] ^= R(x[10]+x[ 6], 7);  x[ 2] ^= R(x[14]+x[10], 9);
-    x[ 6] ^= R(x[ 2]+x[14],13);  x[10] ^= R(x[ 6]+x[ 2],18);
+    x[14] ^= R(x[10] + x[6], 7);
+    x[2] ^= R(x[14] + x[10], 9);
+    x[6] ^= R(x[2] + x[14], 13);
+    x[10] ^= R(x[6] + x[2], 18);
 
-    x[ 3] ^= R(x[15]+x[11], 7);  x[ 7] ^= R(x[ 3]+x[15], 9);
-    x[11] ^= R(x[ 7]+x[ 3],13);  x[15] ^= R(x[11]+x[ 7],18);
+    x[3] ^= R(x[15] + x[11], 7);
+    x[7] ^= R(x[3] + x[15], 9);
+    x[11] ^= R(x[7] + x[3], 13);
+    x[15] ^= R(x[11] + x[7], 18);
 
     /* Operate on rows. */
-    x[ 1] ^= R(x[ 0]+x[ 3], 7);  x[ 2] ^= R(x[ 1]+x[ 0], 9);
-    x[ 3] ^= R(x[ 2]+x[ 1],13);  x[ 0] ^= R(x[ 3]+x[ 2],18);
+    x[1] ^= R(x[0] + x[3], 7);
+    x[2] ^= R(x[1] + x[0], 9);
+    x[3] ^= R(x[2] + x[1], 13);
+    x[0] ^= R(x[3] + x[2], 18);
 
-    x[ 6] ^= R(x[ 5]+x[ 4], 7);  x[ 7] ^= R(x[ 6]+x[ 5], 9);
-    x[ 4] ^= R(x[ 7]+x[ 6],13);  x[ 5] ^= R(x[ 4]+x[ 7],18);
+    x[6] ^= R(x[5] + x[4], 7);
+    x[7] ^= R(x[6] + x[5], 9);
+    x[4] ^= R(x[7] + x[6], 13);
+    x[5] ^= R(x[4] + x[7], 18);
 
-    x[11] ^= R(x[10]+x[ 9], 7);  x[ 8] ^= R(x[11]+x[10], 9);
-    x[ 9] ^= R(x[ 8]+x[11],13);  x[10] ^= R(x[ 9]+x[ 8],18);
+    x[11] ^= R(x[10] + x[9], 7);
+    x[8] ^= R(x[11] + x[10], 9);
+    x[9] ^= R(x[8] + x[11], 13);
+    x[10] ^= R(x[9] + x[8], 18);
 
-    x[12] ^= R(x[15]+x[14], 7);  x[13] ^= R(x[12]+x[15], 9);
-    x[14] ^= R(x[13]+x[12],13);  x[15] ^= R(x[14]+x[13],18);
-#undef R
+    x[12] ^= R(x[15] + x[14], 7);
+    x[13] ^= R(x[12] + x[15], 9);
+    x[14] ^= R(x[13] + x[12], 13);
+    x[15] ^= R(x[14] + x[13], 18);
+    #undef R
   }
 
   #pragma clang loop unroll(full)
@@ -142,13 +152,11 @@ salsa20_8(uint32_t B[16])
  * bytes in length; the output Bout must also be the same size.  The
  * temporary space X must be 64 bytes.
  */
-static void
-blockmix_salsa8(const uint32_t * Bin, uint32_t * Bout, uint32_t * X, size_t r)
-{
+static void blockmix_salsa8(const uint32_t *Bin, uint32_t *Bout, uint32_t *X, size_t r) {
   /* 1: X <-- B_{2r - 1} */
   #pragma clang loop unroll(full)
   for (uint8_t i = 0; i < 8; i++) {
-    ((uint64_t*)X)[i] = ((uint64_t*)&Bin[(2 * r - 1) * 16])[i];
+    ((uint64_t *)X)[i] = ((uint64_t *)&Bin[(2 * r - 1) * 16])[i];
   }
 
   /* 2: for i = 0 to 2r - 1 do */
@@ -156,7 +164,7 @@ blockmix_salsa8(const uint32_t * Bin, uint32_t * Bout, uint32_t * X, size_t r)
     /* 3: X <-- H(X \xor B_i) */
     #pragma clang loop unroll(full)
     for (uint8_t j = 0; j < 8; j++) {
-      ((uint64_t*)X)[j] ^= ((uint64_t*)&Bin[i * 16])[j];
+      ((uint64_t *)X)[j] ^= ((uint64_t *)&Bin[i * 16])[j];
     }
     salsa20_8(X);
 
@@ -164,13 +172,13 @@ blockmix_salsa8(const uint32_t * Bin, uint32_t * Bout, uint32_t * X, size_t r)
     /* 6: B' <-- (Y_0, Y_2 ... Y_{2r-2}, Y_1, Y_3 ... Y_{2r-1}) */
     #pragma clang loop unroll(full)
     for (uint8_t j = 0; j < 8; j++) {
-      ((uint64_t*)&Bout[i * 8])[j] = ((uint64_t*)X)[j];
+      ((uint64_t *)&Bout[i * 8])[j] = ((uint64_t *)X)[j];
     }
 
     /* 3: X <-- H(X \xor B_i) */
     #pragma clang loop unroll(full)
     for (uint8_t j = 0; j < 8; j++) {
-      ((uint64_t*)X)[j] ^= ((uint64_t*)&Bin[i * 16 + 16])[j];
+      ((uint64_t *)X)[j] ^= ((uint64_t *)&Bin[i * 16 + 16])[j];
     }
     salsa20_8(X);
 
@@ -178,7 +186,7 @@ blockmix_salsa8(const uint32_t * Bin, uint32_t * Bout, uint32_t * X, size_t r)
     /* 6: B' <-- (Y_0, Y_2 ... Y_{2r-2}, Y_1, Y_3 ... Y_{2r-1}) */
     #pragma clang loop unroll(full)
     for (uint8_t j = 0; j < 8; j++) {
-      ((uint64_t*)&Bout[i * 8 + r * 16])[j] = ((uint64_t*)X)[j];
+      ((uint64_t *)&Bout[i * 8 + r * 16])[j] = ((uint64_t *)X)[j];
     }
   }
 }
@@ -187,10 +195,8 @@ blockmix_salsa8(const uint32_t * Bin, uint32_t * Bout, uint32_t * X, size_t r)
  * integerify(B, r):
  * Return the result of parsing B_{2r-1} as a little-endian integer.
  */
-static inline uint64_t
-integerify(const void * B, size_t r)
-{
-  const uint32_t * X = (const void *)((uintptr_t)(B) + (2 * r - 1) * 64);
+static inline uint64_t integerify(const void *B, size_t r) {
+  const uint32_t *X = (const void *)((uintptr_t)(B) + (2 * r - 1) * 64);
 
   return (((uint64_t)(X[1]) << 32) + X[0]);
 }
@@ -203,13 +209,11 @@ integerify(const void * B, size_t r)
  * power of 2 greater than 1.  The arrays B, V, and XY must be aligned to a
  * multiple of 64 bytes.
  */
-void
-smix(uint8_t * B, size_t r, uint64_t N, void * _V, void * XY)
-{
-  uint32_t * X = XY;
-  uint32_t * Y = (void *)((uint8_t *)(XY) + 128 * r);
-  uint32_t * Z = (void *)((uint8_t *)(XY) + 256 * r);
-  uint32_t * V = _V;
+void smix(uint8_t *B, size_t r, uint64_t N, void *_V, void *XY) {
+  uint32_t *X = XY;
+  uint32_t *Y = (void *)((uint8_t *)(XY) + 128 * r);
+  uint32_t *Z = (void *)((uint8_t *)(XY) + 256 * r);
+  uint32_t *V = _V;
 
   /* 1: X <-- B */
   for (uint32_t k = 0; k < 32 * r; k++) {
@@ -221,8 +225,8 @@ smix(uint8_t * B, size_t r, uint64_t N, void * _V, void * XY)
     /* 3: V_i <-- X */
 
     for (uint32_t j = 0; j < r; j++) {
-      uint64_t *dest = &(((uint64_t*)&V[i * (32 * r)])[j * 16]);
-      uint64_t *src = &(((uint64_t*)X)[j * 16]);
+      uint64_t *dest = &(((uint64_t *)&V[i * (32 * r)])[j * 16]);
+      uint64_t *src = &(((uint64_t *)X)[j * 16]);
       #pragma clang loop unroll(full)
       for (uint8_t jj = 0; jj < 16; jj++) {
         dest[jj] = src[jj];
@@ -233,8 +237,8 @@ smix(uint8_t * B, size_t r, uint64_t N, void * _V, void * XY)
 
     /* 3: V_i <-- X */
     for (uint32_t j = 0; j < r; j++) {
-      uint64_t *dest = &(((uint64_t*)&V[(i + 1) * (32 * r)])[j * 16]);
-      uint64_t *src = &(((uint64_t*)Y)[j * 16]);
+      uint64_t *dest = &(((uint64_t *)&V[(i + 1) * (32 * r)])[j * 16]);
+      uint64_t *src = &(((uint64_t *)Y)[j * 16]);
       #pragma clang loop unroll(full)
       for (uint8_t jj = 0; jj < 16; jj++) {
         dest[jj] = src[jj];
@@ -252,8 +256,8 @@ smix(uint8_t * B, size_t r, uint64_t N, void * _V, void * XY)
 
     /* 8: X <-- H(X \xor V_j) */
     for (uint32_t z = 0; z < r; z++) {
-      uint64_t *dest = &(((uint64_t*)X)[z * 16]);
-      uint64_t *src = &(((uint64_t*)&V[j * (32 * r)])[z * 16]);
+      uint64_t *dest = &(((uint64_t *)X)[z * 16]);
+      uint64_t *src = &(((uint64_t *)&V[j * (32 * r)])[z * 16]);
       #pragma clang loop unroll(full)
       for (uint8_t zz = 0; zz < 16; zz++) {
         dest[zz] ^= src[zz];
@@ -266,8 +270,8 @@ smix(uint8_t * B, size_t r, uint64_t N, void * _V, void * XY)
 
     /* 8: X <-- H(X \xor V_j) */
     for (uint32_t z = 0; z < r; z++) {
-      uint64_t *dest = &(((uint64_t*)Y)[z * 16]);
-      uint64_t *src = &(((uint64_t*)&V[j * (32 * r)])[z * 16]);
+      uint64_t *dest = &(((uint64_t *)Y)[z * 16]);
+      uint64_t *src = &(((uint64_t *)&V[j * (32 * r)])[z * 16]);
       #pragma clang loop unroll(full)
       for (uint8_t zz = 0; zz < 16; zz++) {
         dest[zz] ^= src[zz];
