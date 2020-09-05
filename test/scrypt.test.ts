3d27d16443ddcd5bf684ff31fd2e4355766237ba
Dani Biró <danibiro93@gmail.com>
2020-09-05 15:05:08 +0300

    Add official scrypt test vectors

--- test/scrypt.test.ts
+++ test/scrypt.test.ts
@@ -39,3 +39,21 @@ test('scrypt', async () => {
     await hash('abc', '12345678', 64, 27, 67, 17, 'hex'),
   ).toBe('6e018c6f21b5647b3ce0c1c65ea14961f4');
 });
+
+test('scrypt official test vectors', async () => {
+  expect(
+    await hash('', '', 16, 1, 1, 64, 'hex'),
+  ).toBe('77d6576238657b203b19ca42c18a0497f16b4844e3074ae8dfdffa3fede21442fcd0069ded0948f8326a753a0fc81f17e8d3e0fb2e0d3628cf35e20c38d18906');
+
+  expect(
+    await hash('password', 'NaCl', 1024, 8, 16, 64, 'hex'),
+  ).toBe('fdbabe1c9d3472007856e7190d01e9fe7c6ad7cbc8237830e77376634b3731622eaf30d92e22a3886ff109279d9830dac727afb94a83ee6d8360cbdfa2cc0640');
+
+  expect(
+    await hash('pleaseletmein', 'SodiumChloride', 16384, 8, 1, 64, 'hex'),
+  ).toBe('7023bdcb3afd7348461c06cd81fd38ebfda8fbba904f8e3ea9b543f6545da1f2d5432955613f0fcf62d49705242a9af9e61e85dc0d651e40dfcf017b45575887');
+
+  expect(
+    await hash('pleaseletmein', 'SodiumChloride', 1048576, 8, 1, 64, 'hex'),
+  ).toBe('2101cb9b6a511aaeaddbbe09cf70f881ec568d574a2ffd4dabe5ee9820adaa478e56fd8f4ba5d09ffa1c6d927c40f4c337304049e8a952fbcbf45c6fa77a41a4');
+});
