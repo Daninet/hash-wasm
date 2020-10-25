aec79103b107b01d77fce38c071e1ecfe8d94cad
Dani Biró <danibiro93@gmail.com>
2020-10-25 11:00:51 +0200

    Add file size calculations

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -13,42 +13,56 @@ LDFLAGS="-Wl,--strip-all -Wl,--initial-memory=262144 -Wl,--max-memory=262144 -Wl
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/blake2b.wasm /app/src/blake2b.c
 sha1sum /app/wasm/blake2b.wasm
+stat -c "%n size: %s bytes" /app/wasm/blake2b.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/blake2s.wasm /app/src/blake2s.c
 sha1sum /app/wasm/blake2s.wasm
+stat -c "%n size: %s bytes" /app/wasm/blake2s.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/crc32.wasm /app/src/crc32.c
 sha1sum /app/wasm/crc32.wasm
+stat -c "%n size: %s bytes" /app/wasm/crc32.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/md4.wasm /app/src/md4.c
 sha1sum /app/wasm/md4.wasm
+stat -c "%n size: %s bytes" /app/wasm/md4.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/md5.wasm /app/src/md5.c
 sha1sum /app/wasm/md5.wasm
+stat -c "%n size: %s bytes" /app/wasm/md5.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/ripemd160.wasm /app/src/ripemd160.c
 sha1sum /app/wasm/ripemd160.wasm
+stat -c "%n size: %s bytes" /app/wasm/ripemd160.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/sha1.wasm /app/src/sha1.c
 sha1sum /app/wasm/sha1.wasm
+stat -c "%n size: %s bytes" /app/wasm/sha1.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/sha256.wasm /app/src/sha256.c
 sha1sum /app/wasm/sha256.wasm
+stat -c "%n size: %s bytes" /app/wasm/sha256.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/sha512.wasm /app/src/sha512.c
 sha1sum /app/wasm/sha512.wasm
+stat -c "%n size: %s bytes" /app/wasm/sha512.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/sha3.wasm /app/src/sha3.c
 sha1sum /app/wasm/sha3.wasm
+stat -c "%n size: %s bytes" /app/wasm/sha3.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/sm3.wasm /app/src/sm3.c
 sha1sum /app/wasm/sm3.wasm
+stat -c "%n size: %s bytes" /app/wasm/sm3.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/whirlpool.wasm /app/src/whirlpool.c
 sha1sum /app/wasm/whirlpool.wasm
+stat -c "%n size: %s bytes" /app/wasm/whirlpool.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/xxhash32.wasm /app/src/xxhash32.c
 sha1sum /app/wasm/xxhash32.wasm
+stat -c "%n size: %s bytes" /app/wasm/xxhash32.wasm
 
 clang ${CFLAGS} ${LDFLAGS} -o /app/wasm/xxhash64.wasm /app/src/xxhash64.c
 sha1sum /app/wasm/xxhash64.wasm
+stat -c "%n size: %s bytes" /app/wasm/xxhash64.wasm
