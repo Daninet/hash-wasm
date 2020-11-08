de62eee4e80e3a73d9685af1773aa864f962d58f
Dani Biró <danibiro93@gmail.com>
2020-11-08 11:59:25 +0200

    Create minified bundles from each algorithm

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,31 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+import gzipPlugin from 'rollup-plugin-gzip';
+import license from 'rollup-plugin-license';
 
-export default {
+const ALGORITHMS = [
+  'argon2', 'bcrypt', 'blake2b', 'blake2s', 'crc32', 'hmac', 'keccak', 'md4', 'md5',
+  'pbkdf2', 'ripemd160', 'scrypt', 'sha1', 'sha3', 'sha224', 'sha256', 'sha384', 'sha512',
+  'sm3', 'whirlpool', 'xxhash32', 'xxhash64',
+];
+
+const TERSER_CONFIG = {
+  output: {
+    comments: false,
+  },
+};
+
+const LICENSE_CONFIG = {
+  banner: {
+    commentStyle: 'ignored',
+    content: `hash-wasm (https://www.npmjs.com/package/hash-wasm)
+    (c) Dani Biro
+    @license MIT`,
+  },
+};
+
+const MAIN_BUNDLE_CONFIG = {
   input: 'lib/index.ts',
   output: [
     {
@@ -17,5 +41,52 @@ export default {
   plugins: [
     json(),
     typescript(),
+    license(LICENSE_CONFIG),
   ],
 };
+
+const MINIFIED_MAIN_BUNDLE_CONFIG = {
+  input: 'lib/index.ts',
+  output: [
+    {
+      file: 'dist/index.umd.min.js',
+      name: 'hashwasm',
+      format: 'umd',
+    },
+    {
+      file: 'dist/index.esm.min.js',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    json(),
+    typescript(),
+    terser(TERSER_CONFIG),
+    license(LICENSE_CONFIG),
+  ],
+};
+
+const INDIVIDUAL_BUNDLE_CONFIG = (algorithm) => ({
+  input: `lib/${algorithm}.ts`,
+  output: [
+    {
+      file: `dist/${algorithm}.umd.min.js`,
+      name: 'hashwasm',
+      format: 'umd',
+      extend: true,
+    },
+  ],
+  plugins: [
+    json(),
+    typescript(),
+    terser(TERSER_CONFIG),
+    license(LICENSE_CONFIG),
+    // gzipPlugin(),
+  ],
+});
+
+export default [
+  MAIN_BUNDLE_CONFIG,
+  MINIFIED_MAIN_BUNDLE_CONFIG,
+  ...ALGORITHMS.map(INDIVIDUAL_BUNDLE_CONFIG),
+];
