83f8ece493f0ca7b32c554430350f19d833e35c3
Dani Biró <danibiro93@gmail.com>
2020-11-08 11:37:40 +0200

    Remove emscripten-based build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p dist
 mkdir -p wasm
 
 npm run eslint
 
+if [[ "$(docker images -q clang:latest 2> /dev/null)" == "" ]]; then
+  docker build -f scripts/Dockerfile -t clang:latest .
+fi
+
 docker run \
   --rm \
   -v $(pwd):/app \
   -u $(id -u):$(id -g) \
-  trzeci/emscripten-upstream:1.39.15 \
-  sh -c /app/scripts/build_all.sh
+  clang:latest \
+  bash -c /app/scripts/build_clang.sh
 
 # node scripts/optimize
 node scripts/make_json
