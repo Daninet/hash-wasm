6ce3760aee50b07662b690ee22e1edc0c63623d0
Dani Biró <danibiro93@gmail.com>
2021-06-04 21:59:14 +0300

    Use hash-wasm label for docker build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,15 +7,15 @@ mkdir -p wasm
 
 npm run eslint
 
-if [[ "$(docker images -q clang:latest 2> /dev/null)" == "" ]]; then
-  docker build -f scripts/Dockerfile -t clang:latest .
+if [[ "$(docker images -q clang:hash-wasm 2> /dev/null)" == "" ]]; then
+  docker build -f scripts/Dockerfile -t clang:hash-wasm .
 fi
 
 docker run \
   --rm \
   -v $(pwd):/app \
   -u $(id -u):$(id -g) \
-  clang:latest \
+  clang:hash-wasm \
   make -f /app/scripts/Makefile-clang --silent --always-make --output-sync=target -j8 all
 
 # node scripts/optimize
